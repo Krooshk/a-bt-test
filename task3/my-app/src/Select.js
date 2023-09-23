88afe0df78c8d92704aaacdcd19976f0364ed899
Krooshk <kirill.Krush@mail.ru>
2023-09-23 17:31:03 +0300

    done task №3

---
+++ task3/my-app/src/Select.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function Select({ data, filter, onSelect }) {
+	const [filteredOptions, setFilteredOptions] = useState([]);
+	const [inputValue, setInputValue] = useState('');
+	const [isOpen, setIsOpen] = useState(false);
+	const wrapperRef = useRef(null);
+	data = filter(data).map(el => el.name);
+
+	useEffect(() => {
+		const filtered = data.filter(option =>
+			option.toLowerCase().includes(inputValue.toLowerCase())
+		);
+		setFilteredOptions(filtered);
+	}, [inputValue]);
+
+	const handleInputChange = event => {
+		setInputValue(event.target.value);
+		setIsOpen(true);
+	};
+
+	const handleSelectOption = option => {
+		setInputValue(option);
+		onSelect(option);
+	};
+
+	const handleClickOutside = event => {
+		if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+			setIsOpen(false);
+		}
+	};
+
+	const handleDropdownClick = () => {
+		setIsOpen(true);
+	};
+
+	useEffect(() => {
+		document.addEventListener('click', handleClickOutside);
+		return () => {
+			document.removeEventListener('click', handleClickOutside);
+		};
+	}, []);
+
+	return (
+		<div ref={wrapperRef} className='widget'>
+			<input
+				type="text"
+				value={inputValue}
+				onChange={handleInputChange}
+				placeholder="Введите значение"
+				onClick={handleDropdownClick}
+			/>
+			{isOpen && <ul>
+				{filteredOptions.map((option, index) => (
+					<li key={index} onClick={() => handleSelectOption(option)}>
+						{option}
+					</li>
+				))}
+			</ul>}
+		</div>
+	);
+}
+
+export default Select;
--- task3/my-app/src/index.js
+++ task3/my-app/src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Select from './Select';
+
+
+const data = require('./data.json');
+
+function onlyDog(data) {
+	return data.filter(el => el.type === "dog")
+}
+function onlyCat(data) {
+	return data.filter(el => el.type === "cat")
+}
+
+function App() {
+	const [selectedValue, setSelectedValue] = useState('');
+
+	const handleSelect = value => {
+		setSelectedValue(value);
+	};
+
+	return (
+		<div>
+			<h3>All items</h3>
+			<Select data={data} onSelect={handleSelect} filter={(x) => x} />
+			<h3>only dogs</h3>
+			<Select data={data} onSelect={handleSelect} filter={onlyDog} />
+			<h3>only cats</h3>
+			<Select data={data} onSelect={handleSelect} filter={onlyCat} />
+		</div>
+	);
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+	<React.StrictMode>
+		<App></App>
+	</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
